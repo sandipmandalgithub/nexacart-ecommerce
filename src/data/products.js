ba65ea79937e7d4c29903af18626bd40cc3983f0
sandipmandalgithub <sandipmandal4055@gmail.com>
2026-09-15 20:19:34 +0530

    Add dynamic product details and shared product data

---
+++ src/data/products.js
@@ -0,0 +1,25 @@
+const products = [
+  {
+    id: 1,
+    name: "Wireless Headphones",
+    price: 1999,
+    image: "https://tse2.mm.bing.net/th/id/OIP.U57NAojmfYXq3vIQ7Rk0YQHaHa?r=0&rs=1&pid=ImgDetMain&o=7&rm=3/200x200",
+    description: "High-quality wireless headphones with clear sound and comfortable design."
+  },
+  {
+    id: 2,
+    name: "Smart Watch",
+    price: 2999,
+    image: "https://th.bing.com/th/id/OIP.vdg6G-Iqm44cFkNa1gxhyAHaHa?w=193&h=193&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3/200x200",
+    description: "A stylish smart watch for tracking your daily activities and staying connected."
+  },
+  {
+    id: 3,
+    name: "Keyboard",
+    price: 1499,
+    image: "https://th.bing.com/th/id/OIP.pqKuvHNrSVU1q_B15x5eLAHaFI?w=276&h=191&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3/200x200",
+    description: "A comfortable keyboard designed for everyday typing and productivity."
+  }
+]
+
+export default products
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,12 +1,50 @@
-import { useParams } from "react-router-dom"
+import {Link, useParams } from "react-router-dom"
+import { useDispatch } from "react-redux"
+
+import { addToCart } from "../../redux/slice.js"
+import products from "../data/products"
 
 const ProductDetails = () => {
   const { id } = useParams()
+  const dispatch = useDispatch()
+
+  const product = products.find(
+    (item) => item.id === Number(id)
+  )
+
+  if (!product) {
+    return <h1>Product Not Found</h1>
+  }
 
   return (
     <div>
-      <h1>Product Details</h1>
-      <p>Product ID: {id}</p>
+      <img
+        src={product.image}
+        alt={product.name}
+      />
+
+      <h1>
+        {product.name}
+      </h1>
+
+      <h2>
+        ₹{product.price}
+      </h2>
+
+      <p>
+        {product.description}
+      </p>
+
+      <button
+        onClick={() => dispatch(addToCart(product))}
+      >
+        Add To Cart
+      </button>
+
+      <Link to="/cart">
+      Go To Cart
+      </Link> 
+
     </div>
   )
 }
