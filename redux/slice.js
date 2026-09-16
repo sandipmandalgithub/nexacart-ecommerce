2e4011066f39e3e7b6c114b780c2fe7ab047bb30
sandipmandalgithub <sandipmandal4055@gmail.com>
2026-09-16 07:43:15 +0530

    Add cart quantity and subtotal features

--- redux/slice.js
+++ redux/slice.js
@@ -1,10 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit"
 
-
 const initialState = {
   cartItems: []
 }
-
 
 const cartSlice = createSlice({
   name: "cart",
@@ -12,9 +10,39 @@ const cartSlice = createSlice({
   initialState,
 
   reducers: {
-
     addToCart: (state, action) => {
-      state.cartItems.push(action.payload)
+      const existingItem = state.cartItems.find(
+        (item) => item.id === action.payload.id
+      )
+
+      if (existingItem) {
+        existingItem.quantity += 1
+      } else {
+        state.cartItems.push({
+          ...action.payload,
+          quantity: 1
+        })
+      }
+    },
+
+    increaseQuantity: (state, action) => {
+      const item = state.cartItems.find(
+        (item) => item.id === action.payload
+      )
+
+      if (item) {
+        item.quantity += 1
+      }
+    },
+
+    decreaseQuantity: (state, action) => {
+      const item = state.cartItems.find(
+        (item) => item.id === action.payload
+      )
+
+      if (item && item.quantity > 1) {
+        item.quantity -= 1
+      }
     },
 
     removeFromCart: (state, action) => {
@@ -26,16 +54,15 @@ const cartSlice = createSlice({
     clearCart: (state) => {
       state.cartItems = []
     }
-
   }
 })
 
-
 export const {
   addToCart,
+  increaseQuantity,
+  decreaseQuantity,
   removeFromCart,
   clearCart
 } = cartSlice.actions
-
 
 export default cartSlice.reducer
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,88 +1,104 @@
 import { useDispatch, useSelector } from "react-redux"
-import { removeFromCart, clearCart } from "../../redux/slice.js"
 
+import {
+  increaseQuantity,
+  decreaseQuantity,
+  removeFromCart,
+  clearCart
+} from "../../redux/slice.js"
 
 const Cart = () => {
-
   const cartItems = useSelector(
     (state) => state.cart.cartItems
   )
 
   const dispatch = useDispatch()
-
+  const totalPrice = cartItems.reduce(
+  (total, item) => total + item.price * item.quantity,
+  0
+)
 
   return (
     <div className="cart-container">
-
       <h2>
         Shopping Cart
       </h2>
 
+      {cartItems.length === 0 ? (
+        <p>
+          Your cart is empty
+        </p>
+      ) : (
+        <>
+          {cartItems.map((item) => (
+            <div
+              className="cart-item"
+              key={item.id}
+            >
+              <img
+                src={item.image}
+                alt={item.name}
+              />
 
-      {
-        cartItems.length === 0 ? (
+              <div>
+                <h3>
+                  {item.name}
+                </h3>
 
-          <p>
-            Your cart is empty
-          </p>
+                <p>
+                  ₹{item.price}
+                </p>
+                <p>
+                   Subtotal: ₹{item.price * item.quantity}
+                </p>
 
-        ) : (
+                <div>
+                  <button
+                    onClick={() =>
+                      dispatch(decreaseQuantity(item.id))
+                    }
+                  >
+                    -
+                  </button>
 
-          <>
-            {
-              cartItems.map((item) => (
+                  <span>
+                    {item.quantity}
+                  </span>
 
-                <div
-                  className="cart-item"
-                  key={item.id}
-                >
-
-                  <img
-                    src={item.image}
-                    alt={item.name}
-                  />
-
-                  <div>
-
-                    <h3>
-                      {item.name}
-                    </h3>
-
-                    <p>
-                      ₹{item.price}
-                    </p>
-
-                    <button
-                      onClick={() =>
-                        dispatch(removeFromCart(item.id))
-                      }
-                    >
-                      Remove
-                    </button>
-
-                  </div>
-
+                  <button
+                    onClick={() =>
+                      dispatch(increaseQuantity(item.id))
+                    }
+                  >
+                    +
+                  </button>
                 </div>
 
-              ))
-            }
+                <button
+                  onClick={() =>
+                    dispatch(removeFromCart(item.id))
+                  }
+                >
+                  Remove
+                </button>
+              </div>
+            </div>
+          ))}
 
+        <h3>
+        Total: ₹{totalPrice}
+        </h3>
+        <button
+            onClick={() => dispatch(clearCart())}
+          >
+            Clear Cart
+        </button>
 
-            <button
-              onClick={() => dispatch(clearCart())}
-            >
-              Clear Cart
-            </button>
-
-          </>
-
-        )
-      }
-
+        
+        </>
+      )}
     </div>
   )
 }
 
-
 export default Cart
-
